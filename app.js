dd40a978bff4c46c56d15d1cbccc67bcfc453435
Dispellizer <Dispellizer@naver.com>
2020-02-17 11:25:33 +0900

    Revert "2.9"
    This reverts commit ffe4d1c3da73985f337bc8053038a7e3c2a87861.

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import userRouter from "./routers/userRouter";
-import videoRouter from "./routers/videoRouter";
-import globalRouter from "./routers/globalRouter";
+import { userRouter } from "./router";
 const app = express();
+
+const handleHome = (req, res) => res.send("Hello from my home");
+
+const handleProfile = (req, res) => res.send("You are on my profile");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -14,8 +16,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.use("/", globalRouter);
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
 app.use("/user", userRouter);
-app.use("/video", videoRouter);
 
 export default app;
