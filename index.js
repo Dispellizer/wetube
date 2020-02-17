634948f4b16160e16b0c87e8b8415059d4f3832c
Dispellizer <Dispellizer@naver.com>
2020-02-17 10:05:40 +0900

    2.6 middleware
    middleware part one

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+// const express = require("express");
 const app = express();
 
 const PORT = 4000;
@@ -7,11 +8,19 @@ const handleListening = () =>
   console.log(`Listening on: http://localhost:${PORT}`);
 
 const handleHome = (req, res) => res.send("Hello from my home");
+// arrow function(ES6)
 // function handelHome(req, res) {
 //   res.send("Hello from my home");
 // }
 
 const handleProfile = (req, res) => res.send("You are on my profile");
+
+const betweenHome = (req, res, next) => {
+  console.log("I'm between");
+  next();
+};
+
+app.use(betweenHome);
 
 app.get("/", handleHome);
 
