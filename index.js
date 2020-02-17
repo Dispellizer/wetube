1721a1cc86af2490e7b32d967b5c27fabb11914a
Dispellizer <Dispellizer@naver.com>
2020-02-17 10:21:01 +0900

    2.7 middleware part two
    morgan, helmet, cookie-pareser, body-parser

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 // const express = require("express");
 const app = express();
 
@@ -15,12 +19,11 @@ const handleHome = (req, res) => res.send("Hello from my home");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
 
-const betweenHome = (req, res, next) => {
-  console.log("I'm between");
-  next();
-};
-
-app.use(betweenHome);
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get("/", handleHome);
 
