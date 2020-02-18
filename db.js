4c763fcd3a4c1b0a4a42c4054f2030ded948f77a
Dispellizer <Dispellizer@naver.com>
2020-02-18 12:17:44 +0900

    2.21 Home Controller

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+  {
+    id: 332443,
+    title: "Video awesome",
+    discription: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creators: {
+      id: 453421,
+      name: "Dispellizer",
+      email: "Dispellizer@naver.com"
+    }
+  },
+  {
+    id: 43534,
+    title: "Video super",
+    discription: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creators: {
+      id: 453421,
+      name: "Dispellizer",
+      email: "Dispellizer@naver.com"
+    }
+  },
+  {
+    id: 565643,
+    title: "Video nice",
+    discription: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creators: {
+      id: 453421,
+      name: "Dispellizer",
+      email: "Dispellizer@naver.com"
+    }
+  },
+  {
+    id: 11111,
+    title: "Video perfact",
+    discription: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creators: {
+      id: 453421,
+      name: "Dispellizer",
+      email: "Dispellizer@naver.com"
+    }
+  }
+];
