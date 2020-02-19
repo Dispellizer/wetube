3b0f39f28dd69c5b466a5b7b2e5e002bcc68a816
Dispellizer <Dispellizer@naver.com>
2020-02-19 15:20:03 +0900

    3.1 Connecting to MongoDB

--- db.js
+++ db.js
@@ -1,54 +1,14 @@
-export const videos = [
-  {
-    id: 332443,
-    title: "Video awesome",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creators: {
-      id: 453421,
-      name: "Dispellizer",
-      email: "Dispellizer@naver.com"
-    }
-  },
-  {
-    id: 43534,
-    title: "Video super",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creators: {
-      id: 453421,
-      name: "Dispellizer",
-      email: "Dispellizer@naver.com"
-    }
-  },
-  {
-    id: 565643,
-    title: "Video nice",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creators: {
-      id: 453421,
-      name: "Dispellizer",
-      email: "Dispellizer@naver.com"
-    }
-  },
-  {
-    id: 11111,
-    title: "Video perfact",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creators: {
-      id: 453421,
-      name: "Dispellizer",
-      email: "Dispellizer@naver.com"
-    }
-  }
-];
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/we-tube", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ Connected to DB");
+const handleError = error => console.log(`❌ Error on DB Connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
