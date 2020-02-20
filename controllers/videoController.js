164f94547ec0f4a512e35df621a38e98c312f75e
Dispellizer <Dispellizer@naver.com>
2020-02-20 22:09:35 +0900

    3.10 Deleting a Video

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -39,7 +39,7 @@ export const videoDetail = async (req, res) => {
   } = req;
   try {
     const video = await Video.findById(id);
-    res.render("videoDetail", { pageTitle: "Video Detail", video });
+    res.render("videoDetail", { pageTitle: video.title, video });
   } catch (error) {
     console.log(Error);
     res.redirect(routes.home);
@@ -73,5 +73,13 @@ export const postEditVideo = async (req, res) => {
   }
 };
 
-export const deleteVideo = (req, res) =>
+export const deleteVideo = async (req, res) => {
+  const {
+    params: { id }
+  } = req;
+  try {
+    await Video.findOneAndRemove({ _id: id });
+  } catch (error) {}
+  res.redirect(routes.home);
   res.render("deleteVideo", { pageTitle: "Delete Video" });
+};
