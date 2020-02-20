978ec45eed94ad56091522688b6825a94d780b77
Dispellizer <Dispellizer@naver.com>
2020-02-20 21:56:48 +0900

    3.9 Editing a Video

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,5 +1,6 @@
 import routes from "../routes";
 import Video from "../models/Video";
+import e from "express";
 
 export const home = async (req, res) => {
   try {
@@ -45,7 +46,32 @@ export const videoDetail = async (req, res) => {
     // 존재하지 않는 rul로 가면 home로 redirection
   }
 };
-export const editVideo = (req, res) =>
-  res.render("editVideo", { pageTitle: "Edit Video" });
+export const getEditVideo = async (req, res) => {
+  const {
+    params: { id }
+  } = req;
+  try {
+    const video = await Video.findById(id);
+    res.render("editVideo", { pageTitle: `Edit ${video.title}`, video });
+  } catch (error) {
+    res.redirect(routes.home);
+    // 존재하지 않는 video면 home로 redirection
+  }
+};
+
+export const postEditVideo = async (req, res) => {
+  const {
+    params: { id },
+    body: { title, description }
+  } = req;
+  try {
+    await Video.findOneAndUpdate({ _id: id }, { title, description });
+    res.redirect(routes.videoDetail(id));
+    // title과 description 업데이트 후 변한걸 확인하기 위해서 videoDetail로 redirection
+  } catch (error) {
+    res.redirect(routes.home);
+  }
+};
+
 export const deleteVideo = (req, res) =>
   res.render("deleteVideo", { pageTitle: "Delete Video" });
